5800ac0f64a3b1a511f9028a845bf5e496cfd782
Kennedy Anukam <kennedy.anukam@gmail.com>
2020-11-18 19:12:02 -0800

    React structure

---
+++ frontend/epcr-system/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
