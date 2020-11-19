b81287020eb0ed0c073b3d967e9875df996eb19b
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-11-19 11:50:39 -0800

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,23 +7,7 @@ const auth = require('../middleware/auth');
 const controller = require('./controller');
 
 // Will work for login page for now, can change later:
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  
-  if(!username || !password)
-      return res.status(401).json({ error: 'Username or password field blank');
-  
-  controller.login(username, password, (err, passwordMismatch, token) => {
-    if (err){
-      console.log(err);
-      res.status(500).json({ error: 'Internal error please try again' });
-    }
-    else if (!passwordMismatch)
-      res.status(401).json({ error: 'Incorrect password' });
-    else
-      res.cookie('token', token, { httpOnly: true }).sendStatus(200);
-  })
-})
+router.post("/login", controller.login)
 
 router.get("/", (req, res) => {
   res.status(200).send("test");
