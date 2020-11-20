9c2ffb4886c490e5cd3e06ee508ce504b1a74e6d
Kennedy Anukam <kennedy.anukam@gmail.com>
2020-11-19 19:45:35 -0800

    Defined the schema for a user table in database for testing

--- backend/sql/database.js
+++ backend/sql/database.js
@@ -4,13 +4,23 @@ const con = mysql.createConnection({
     host: process.env.DB_HOSTNAME,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 con.connect(err => {
-    if (err) 
-         return console.log(err);
+    if (err){
+        return console.log(err);
+    }
      console.log("ePCR backend connected to DB");
+    //User table for testing
+    /*
+    var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), email VARCHAR(255), phone VARCHAR(12), name VARCHAR(50))"; 
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+    */
 });
 
 module.exports = con;
