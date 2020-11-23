e830fdddf9debccc5bfba95184864805c42c2b34
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-11-23 11:24:00 -0800

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,10 +1,13 @@
 //All url routes should be directed in this file
 const express = require("express");
 const router = express.Router();
-
 const auth = require('../middleware/auth');
 
 const controller = require('./controller');
+
+const charts = require('./charts/router');
+
+router.use('/charts', charts);
 
 // Will work for login page for now, can change later:
 router.post("/login", controller.login)
@@ -15,4 +18,4 @@ router.get("/", (req, res) => {
   res.status(200).send("test");
 });
 
-module.exports = router
+module.exports = router;
