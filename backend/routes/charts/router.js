ca215193ce9d7ae5fbc292e79f8919cc2da150b7
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-11-23 11:18:37 -0800

    Update router.js

--- backend/routes/charts/router.js
+++ backend/routes/charts/router.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
 
+const auth = require('../../middleware/auth');
+
+const controller = require('./controller');
+
+router.get("/charts", auth, controller.viewAllCharts); // View all charts
+
+router.post("/charts/add", auth, controller.addChart);
+
+module.exports = router;
