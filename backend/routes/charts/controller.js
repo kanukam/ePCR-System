ed99a40d69f34862af4b31a086c37208e60ed5b8
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-11-23 11:25:48 -0800

    Update controller.js

--- backend/routes/charts/controller.js
+++ backend/routes/charts/controller.js
@@ -1,9 +1,9 @@
 function viewAllCharts = (req, res) => {
-
+  //someone else can work on these if they want
 }
 
 function addChart = (req, res) => {
-
+ //someone else can work on these if they want
 }
 
 module.exports = { viewAllCharts, addChart };
