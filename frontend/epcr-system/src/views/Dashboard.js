021450af800b57746ecc4964f432b00a20a57b29
Mason Harlan <mgharlan@uci.edu>
2020-11-29 11:36:16 -0800

    added navbar that shows default profile image and username

--- frontend/epcr-system/src/views/Dashboard.js
+++ frontend/epcr-system/src/views/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { MainContext } from '../Auth'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export class Dashboard extends Component {
 	static contextType = MainContext;
@@ -41,10 +42,19 @@ export class Dashboard extends Component {
         	<React.Fragment>
                 {/* Navigation Bar */}
                 <Navbar bg="light">
-                    <Navbar.Brand>
-                        <img alt="" src="/Rescate-Logo.jpg" width="10%" height="10%" className="d-inline-block align-top"/>{' '}
-                        Rescate de San Carlos
-                    </Navbar.Brand>
+                		<Nav className="ml-auto">
+                			<Navbar.Brand href="#home"> {/*take them to the edit profile page or have a toggle that shows more options*/}
+                				{this.state.username}
+						     	<img
+						        	src="/profile.png"
+						        	width="30"
+						        	height="30"
+						        	className="d-inline-block align-top"
+						        	style={{marginLeft: 10, marginRight: 0}}
+						        	alt="profile"
+						      	/>{' '}
+						    </Navbar.Brand>
+                		</Nav>
                 </Navbar>
             </React.Fragment>
         )
