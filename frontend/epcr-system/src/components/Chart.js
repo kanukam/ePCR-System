a7c14db50d0f5dbde5ae6d8d48d9fc83485a6ef9
Mason Harlan <mgharlan@uci.edu>
2020-11-30 14:29:46 -0800

    updated sidebar

--- frontend/epcr-system/src/components/Chart.js
+++ frontend/epcr-system/src/components/Chart.js
@@ -4,8 +4,6 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import { Link } from 'react-router-dom'
 import AddCall from './Add-call'
 import '../App.css'
@@ -71,48 +69,56 @@ export default class Chart extends Component {
             <React.Fragment>
                 {/* Navigation Bar */}
                 <Navbar bg="light">
-                    <Nav.Item style={{padding: this.state.sidebarSpacing}}>
-                        <Button onClick={this.toggleSidebar}>Hamburger</Button>
+                    <Nav.Item style={{padding: this.state.sidebarSpacing}} className="hamburger-shift">
+                        <Button onClick={this.toggleSidebar}>
+                            <img 
+                                src="/hamburger.svg" 
+                                alt="hamburger" 
+                                width="30" 
+                                height="30" 
+                                style={{marginLef:0, marginRight: 0}}
+                            />
+                        </Button>
                     </Nav.Item>
-            		<Nav className="ml-auto">
-            			<Navbar.Brand href="#home"> {/*take them to the edit profile page or have a toggle that shows more options*/}
-            				{this.state.username}
-					     	<img
-					        	src="/profile.png"
-					        	width="30"
-					        	height="30"
-					        	className="d-inline-block align-top"
-					        	style={{marginLeft: 10, marginRight: 0}}
-					        	alt="profile"
-					      	/>{' '}
-					    </Navbar.Brand>
-            		</Nav>
+                    <Nav className="ml-auto">
+                        <Navbar.Brand href="#home"> {/*take them to the edit profile page or have a toggle that shows more options*/}
+                            {this.state.username}
+                            <img
+                                src="/profile.png"
+                                width="25"
+                                height="25"
+                                className="d-inline-block align-top"
+                                style={{marginLeft: 10, marginRight: 0}}
+                                alt="profile"
+                            />{' '}
+                        </Navbar.Brand>
+                    </Nav>
                 </Navbar>
 
-            	{/* Sidebar */}
+                {/* Sidebar */}
                 { this.state.sidebarHide ?
-            	<Navbar className="col-md-1 d-none d-md-block bg-light sidebar">
-		            <Nav.Item>
+                <Navbar className="col-md-1 d-none d-md-block bg-light sidebar">
+                    <Nav.Item>
                         <Nav.Link as={Link} to="/Dashboard">
-		                	Home
-		                </Nav.Link>
-		            </Nav.Item>
-		            <Nav.Item className="sidebar-section active">
+                            Home
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item  className="sidebar-section active">
                         <Nav.Link style={{color:'white'}} as={Link} to="/Chart">
-		                	Chart
-		                </Nav.Link>
-		            </Nav.Item>
-		            <Nav.Item>
-		                <Nav.Link>
-		                	Link
-		                </Nav.Link>
-		            </Nav.Item>
-		            <Nav.Item>
-		                <Nav.Link>
-		                	Link
-		               	</Nav.Link>
-		            </Nav.Item>
-	            </Navbar>
+                            Chart
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                            Link
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                            Link
+                        </Nav.Link>
+                    </Nav.Item>
+                </Navbar>
                 : null}
 
                 {/* Form */}                
