cd03ecdd119acadaf204b83986a0ab35c58f8dfc
kanukam <49925341+kanukam@users.noreply.github.com>
2020-12-05 00:51:41 -0800

    Update database.js

--- backend/sql/database.js
+++ backend/sql/database.js
@@ -13,14 +13,6 @@ con.connect(err => {
         return console.log(err);
     }
      console.log("ePCR backend connected to DB");
-    //User table for testing
-    /*
-    var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), email VARCHAR(255), phone VARCHAR(12), name VARCHAR(50))"; 
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    });
-    */
 });
 
 module.exports = con;
