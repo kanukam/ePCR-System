f4497e19c778f8109c9bde8cb52afcb7ed93f8f6
kanukam <49925341+kanukam@users.noreply.github.com>
2021-02-05 08:31:17 -0800

    Organized charts backend

--- backend/routes/charts/controller.js
+++ backend/routes/charts/controller.js
@@ -1,51 +1,25 @@
 const db = require('../../sql/database');
-const validate = require('./validate');
+const repo = require('./repository');
 
 function viewAllCharts(req, res) {
-  // currently using user id of 1,
-  // need a get function to get the current user's id
-  // should be similar to the getUsername function
-  const userID = req.user.id;
-  
-  db.query('SELECT * FROM charts;', (err, results, fields) => {
-  //db.query(`SELECT * FROM charts WHERE userID = ${userID}`, (err, results, fields) => {
-    if (err) throw err;
-    res.send(results);
-    //console.log("hello" + results.data['date']);
-  });
+  repo.viewAllCharts((err, data) => {
+    err
+      ? res.status(500).json({ error: err })
+      : res.send(data);
+  })
 }
 
-function addChart(req, resp) {
+function addChart(req, res) {
   const { call, date, times, patient, birth, weight, address, procedure } = req.body;
   var pid = 0;
   // insert into patient table first
-  var body = { name: patient, birth: birth, weight: weight, address: address };
-  var sql = 'INSERT INTO patients SET ?'
-  db.query(sql, body, (err, res) => {
-    if (err) 
-    {
-      console.log(err);
-      return resp.status(500).json({ error: 'Internal error please try again' });
-    }
-    else {
-      console.log(req);
-      // if successful, save patient id
-      pid = res.insertId;
-      // insert into chart table associated with patient
-      var pbody = { call: call, date: date, times: times, patientID: pid, procedures: procedure, userID: req.user.id };
-      var sql = 'INSERT INTO charts SET ?';
-      db.query(sql, pbody, (err, res) => {
-        if (err){
-          console.log(err);
-          return resp.status(500).json({ error: 'Internal error please try again' });
-        }
-        else{
-          resp.status(200).json({ status: 'Successfully added to chart' });
-          console.log(res);
-        }
-      })
-    }
-  });
+  var pbody= { name: patient, birth: birth, weight: weight, address: address };
+  var body = { call: call, date: date, times: times, patientID: pid, procedures: procedure, userID: req.user.id };
+  repo.addChart(body, pbody, (err) => {
+    err
+      ? res.status(500).json({ error: err })
+      : res.status(200).json({ status: 'Successfully added' });
+  })
 }
 
 function updateChart(req, res) {
---
+++ backend/routes/charts/repository.js
@@ -0,0 +1,37 @@
+const db = require('../../sql/database');
+const secret = process.env.JWT_SECRET;
+
+
+function addChart(body, pbody, callback) {
+    // insert into patient table first
+    var sql = 'INSERT INTO patients SET ?';
+    db.query(sql, pbody, (err, res) => {
+        if (err) {
+            return callback(err);
+        }
+        else {
+            // if successful, save patient id
+            pid = res.insertId;
+            var sql = 'INSERT INTO charts SET ?';
+            db.query(sql, body, (err, res) => {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    callback(null);
+                }
+            })
+        }
+    })
+}
+
+function viewAllCharts(callback) {
+    db.query('SELECT * FROM charts;', (err, results) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, results);
+    });
+}
+
+module.exports = { viewAllCharts, addChart};
