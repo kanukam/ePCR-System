d2947521c6b1f7dfb4f1fe30e479ccefb63af4a2
kanukam <49925341+kanukam@users.noreply.github.com>
2021-02-23 22:30:30 -0800

    Refactored

--- frontend/epcr-system/src/Auth.js
+++ frontend/epcr-system/src/Auth.js
@@ -4,5 +4,7 @@ export const MainContext = React.createContext({
     username: "",
     setUsername: () => {},
     auth: false,
+    language: "es",
+    setLanguage: () => {},
     setAuth: () => {}
 })
