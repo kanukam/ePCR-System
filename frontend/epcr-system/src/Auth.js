6b80512dfcbe172bea6f84a8b5eefa212af1c510
kanukam <49925341+kanukam@users.noreply.github.com>
2021-03-03 22:13:44 -0800

    Update Auth.js

--- frontend/epcr-system/src/Auth.js
+++ frontend/epcr-system/src/Auth.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export const MainContext = React.createContext({
     username: "",
-    id: "",
     auth: false,
     language: "es",
     setUsername: () => {},
-    setId: () => { },
     setLanguage: () => {},
     setAuth: () => {}
 })
