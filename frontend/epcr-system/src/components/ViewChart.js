c7920f8c05fb085d7eae0e0d0a0aced2f9570328
kanukam <49925341+kanukam@users.noreply.github.com>
2021-04-03 13:18:51 -0700

    Date Update

--- frontend/epcr-system/src/components/ViewChart.js
+++ frontend/epcr-system/src/components/ViewChart.js
@@ -50,11 +50,11 @@ export default class ViewChart extends Component {
     }
 
     dateOnly(year) {
-        return moment(year).format('DD/MM/YYYY');
+        return moment(year).utc().format('DD/MM/YYYY');
     }
 
     dateAndTime(year) {
-        return moment(year).format('DD/MM/YYYY hh:mm A');
+        return moment(year).utc().format('DD/MM/YYYY hh:mm A');
     }
 
 
