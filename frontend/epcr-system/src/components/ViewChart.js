5e9cd849d281832a51b957cc631cd288ea3cf7cb
kanukam <49925341+kanukam@users.noreply.github.com>
2021-04-18 07:18:46 -0700

    Cleanup

--- frontend/epcr-system/src/components/ViewChart.js
+++ frontend/epcr-system/src/components/ViewChart.js
@@ -17,68 +17,29 @@ export default class ViewChart extends Component {
         this.toggleCollapse = this.toggleCollapse.bind(this);
     }
 
-    componentDidMount() {
-        const url = 'http://localhost:3000/charts/patient/' + this.props.match.params.id;
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        }
-        fetch(url, options)
-            .then((response) => {
-                if(response.ok)
-                    return response.json();
-                else
-                    throw Error("Failed");
-            })
-            .then((data) => {
-                this.setState({chart: data['chart'][0]});
-            })
-            .catch((error) => {
-                console.log(error);
-            }); 
-    }
-
 
     toggleCollapse (){
         this.setState({contentSpacing : (this.state.sidebarHide ? '0 0 0 0' : '0 0 0 150px')})
         this.setState({sidebarHide : !this.state.sidebarHide});
     }
 
-    dateOnly(year) {
-        return moment(year).utc().format('DD/MM/YYYY');
-    }
-
-    dateAndTime(year) {
-        return moment(year).utc().format('DD/MM/YYYY hh:mm A');
-    }
-
-
     render() {
-        // Will error if object destructuring happens and chart isn't sent yet, if statement prevents the error
-        if (this.state.chart){
-            return (
-                <React.Fragment>
-                    <MainNav
-                        username={this.context.username}
-                        viewcharts={true}
-                        sidebarHide={this.state.sidebarHide}
-                        contentSpacing={this.state.contentSpacing}
-                        toggleCollapse={this.toggleCollapse}
-                    />
-                    <div >
-                        <embed src={`http://localhost:3000/charts/${this.props.match.params.id}/pdf?locale=${this.context.language}#scrollbar=1`}  type="application/pdf" width={"100%"} style={{width: "100%", height:"700px"}}>
+        return (
+            <React.Fragment>
+                <MainNav
+                    username={this.context.username}
+                    viewcharts={true}
+                    sidebarHide={this.state.sidebarHide}
+                    contentSpacing={this.state.contentSpacing}
+                    toggleCollapse={this.toggleCollapse}
+                />
+                <div >
+                    <embed src={`http://localhost:3000/charts/${this.props.match.params.id}/pdf?locale=${this.context.language}#scrollbar=1`}  type="application/pdf" width={"100%"} style={{width: "100%", height:"700px"}}>
 
-                        </embed>
-                        {this.state.chart['id'] && <Notes chartId={this.state.chart['id']} />}
-                    </div>
-                </React.Fragment>
-            )
-        }
-        else{
-            return null;
-        }
+                    </embed>
+                    {this.state.chart['id'] && <Notes chartId={this.state.chart['id']} />}
+                </div>
+            </React.Fragment>
+        )
     }
 }
