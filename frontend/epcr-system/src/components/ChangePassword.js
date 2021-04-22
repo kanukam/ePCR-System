968f25ea02f222036f7ddf27a7499d78ad02a3d3
YiJio <zhiyuzhengju@gmail.com>
2021-04-22 12:04:43 -0700

    Changes

--- frontend/epcr-system/src/components/ChangePassword.js
+++ frontend/epcr-system/src/components/ChangePassword.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Jumbotron from 'react-bootstrap/Jumbotron'
 import { MainContext } from '../Auth'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { Link } from "react-router-dom";
