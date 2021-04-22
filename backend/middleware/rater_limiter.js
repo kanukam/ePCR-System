35a9298985ed977c044a66ff7e6997ea000d69f1
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2021-04-22 11:00:54 -0700

    Add files via upload

---
+++ backend/middleware/rater_limiter.js
@@ -0,0 +1,45 @@
+const db = require('../sql/database');
+
+// 10 Attempts are allowed over a period of 1 hour
+const MAX_LOGIN_ATTEMPTS = process.env.MAX_LOGIN_ATTEMPTS || 12;
+const HOURS_TIL_EXPIRY = process.env.HOURS_TIL_EXPIRY || 1;
+
+module.exports = (req, res, next) => {
+    const { ip } = req;
+    let now = new Date();
+    let expiry = new Date(this.getTime() + (HOURS_TIL_EXPIRY*60*60*1000));
+
+    db.query(`SELECT * from ratelimiters where ip='${ip}'`, (err, res) => {
+        if(res.length <= 0){ // IP doesn't exist
+            db.query(`UPDATE ratelimiters SET expiry=${expiry}, hits=1 WHERE ip='${ip}'`)
+            return next();
+        }
+
+        let newhits = res[0].hits || 0;
+        if(res[0].expiry){ // IP exists, has logged expiry
+            let c = new Date(res[0].expiry);
+
+            if(now.getTime() >= c.getTime()){ // Expired time has passed
+                db.query(`UPDATE ratelimiters SET expiry=${expiry}, hits=${newhits + 1} WHERE ip='${ip}'`)
+            }
+            else{
+                db.query(`UPDATE ratelimiters SET hits=${newhits + 1} WHERE ip='${ip}'`)
+            }
+        }
+        else{ // IP exists, no expiry
+            db.query(`UPDATE ratelimiters SET expiry=${expiry}, hits=${newhits + 1} WHERE ip='${ip}'`)
+        }
+
+        if(newhits + 1 > MAX_LOGIN_ATTEMPTS)
+            return res.status(400).send('Access denied');
+        else {
+            next();
+        }
+    })
+}
+
+function decide(res, hits, next){
+    if(hits + 1 > MAX_LOGIN_ATTEMPTS)
+        return res.status(400).send('Access denied');
+    else next();
+}
