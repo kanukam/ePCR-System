693167f97a5dd292056d93109b7771b8568e19b0
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2021-05-04 14:32:32 -0700

    Update AddCall.js

--- frontend/epcr-system/src/components/AddCall.js
+++ frontend/epcr-system/src/components/AddCall.js
@@ -18,9 +18,32 @@ export default class AddCall extends Component {
     }
 
     componentDidMount() {
-        //alert("21-" + Math.floor(Math.random() * 10) + 1);
-        let yearLastTwo = new Date().getFullYear() % 100;
-        
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }
+        fetch("http://localhost:3000/api/charts/number", options)
+            .then((response) => {
+                if (response.ok)
+                    return response.json();
+                else
+                    throw Error("Failed");
+            })
+            .then((data) => {
+                let randNo = Math.floor(Math.random() * 10) + 1);
+                let yearLastTwo = new Date().getFullYear() % 100;
+                let r = "" + data;
+                while (r.length < 4) {
+                    r = "0" + r;
+                }
+                this.props.setState({ino: `${yearLastTwo}-${randNo}-${r}`});
+            })
+            .catch((error) => {
+                console.log(error);
+            }); 
     }
 
     navigate = step => (e) => {
